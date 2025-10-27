e725fe8c9ca929f076496be5f5f257b722177370
Mauricio Sousa <lagrange.tesla@gmail.com>
2025-10-27 14:01:37 -0600

    chore: add migration and factory structure

--- database/factories/StoreFactory.php
+++ database/factories/StoreFactory.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -19,7 +20,8 @@ class StoreFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->name(),
+            'user_id' => User::factory(),
         ];
     }
 }
--- database/migrations/2025_10_27_182947_create_stores_table.php
+++ database/migrations/2025_10_27_182947_create_stores_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('stores', function (Blueprint $table): void {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('name');
             $table->timestamps();
         });
     }
