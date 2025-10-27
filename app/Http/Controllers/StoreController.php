caddaba9fda3750b82a5043576f6b8463970d8b7
Mauricio Sousa <lagrange.tesla@gmail.com>
2025-10-27 16:23:10 -0600

    feat: add logic to store method

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -4,34 +4,42 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use App\Actions\Store\CreateStore;
 use App\Http\Requests\StoreStoreRequest;
 use App\Http\Requests\UpdateStoreRequest;
 use App\Models\Store;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\View\View;
 
 class StoreController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index(): void
+    public function index(): View
     {
-        //
+        return view('store.index', [
+            'stores' => Auth::user()->stores,
+        ]);
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create(): void
+    public function create(): View
     {
-        //
+        return view('store.create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreStoreRequest $request): void
+    public function store(StoreStoreRequest $request, CreateStore $action): RedirectResponse
     {
-        //
+        $action->handle($request->validated(), $request->user());
+
+        return to_route('store.index');
     }
 
     /**
