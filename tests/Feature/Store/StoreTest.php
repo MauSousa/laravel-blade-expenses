044935cb475fe119c845caf349a06fcb7638c065
Mauricio Sousa <lagrange.tesla@gmail.com>
2025-10-27 14:19:25 -0600

    chore: add smoke test for store index route

--- tests/Feature/Store/StoreTest.php
+++ tests/Feature/Store/StoreTest.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use App\Models\Store;
+use App\Models\User;
 
 uses(Illuminate\Foundation\Testing\RefreshDatabase::class);
 
@@ -17,4 +18,20 @@ test('to array', function () {
             'created_at',
             'updated_at',
         ]);
+});
+
+it('belongs to a user', function () {
+    $store = Store::factory()->create();
+
+    expect($store->user)->toBeInstanceOf(User::class);
+});
+
+test('user can view store page', function () {
+    $user = User::factory()->create();
+
+    $this->actingAs($user);
+
+    $response = $this->get(route('stores.index'));
+    $response->assertOk();
+
 });
