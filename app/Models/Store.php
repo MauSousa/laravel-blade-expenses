bbe4c55065e1fb951ac9fa34493e5fbcfd454645
Mauricio Sousa <lagrange.tesla@gmail.com>
2025-10-27 14:18:07 -0600

    chore: add method to get the user that owns the store

--- app/Models/Store.php
+++ app/Models/Store.php
@@ -6,9 +6,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Store extends Model
 {
     /** @use HasFactory<\Database\Factories\StoreFactory> */
     use HasFactory;
+
+    /**
+     * return the user that owns the store
+     *
+     * @return BelongsTo<User, $this>
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
