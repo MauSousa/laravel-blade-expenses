f9ae8c8706cee10e8bc524846f7881d295b5b082
Mauricio Sousa <lagrange.tesla@gmail.com>
2025-10-31 15:46:06 -0600

    chore: add protected properties and eloquent relations

--- app/Models/Expense.php
+++ app/Models/Expense.php
@@ -1,12 +1,42 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Expense extends Model
 {
     /** @use HasFactory<\Database\Factories\ExpenseFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'store_id',
+        'name',
+        'price',
+    ];
+
+    /**
+     * Get the user that owns the expense.
+     *
+     * @return BelongsTo<User, $this>
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the store that owns the expense.
+     *
+     * @return BelongsTo<Store, $this>
+     */
+    public function store(): BelongsTo
+    {
+        return $this->belongsTo(Store::class);
+    }
 }
