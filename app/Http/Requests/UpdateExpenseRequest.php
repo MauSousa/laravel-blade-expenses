426e34d762c3092965b30dffeba444f1c7795074
Mauricio Sousa <lagrange.tesla@gmail.com>
2025-11-01 23:39:58 -0600

    chore: add validation to update expense request

--- app/Http/Requests/UpdateExpenseRequest.php
+++ app/Http/Requests/UpdateExpenseRequest.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace App\Http\Requests;
 
+use Closure;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Validator;
 
 class UpdateExpenseRequest extends FormRequest
 {
@@ -13,7 +15,7 @@ class UpdateExpenseRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return $this->user()->can('update', $this->expense);
     }
 
     /**
@@ -24,7 +26,30 @@ class UpdateExpenseRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => ['required', 'string', 'max:255'],
+            'price' => ['required', 'numeric', 'min:0'],
+            'payment_method' => ['required', 'string', 'max:255', 'in:cash,credit,debit'],
+            'store_id' => ['required', 'exists:stores,id'],
+        ];
+    }
+
+    /**
+     * Checks if the request store belongs to the user.
+     *
+     * @return array<int, Closure>
+     */
+    public function after(): array
+    {
+        return [
+            function (Validator $validator): void {
+                $store = request()->user()->stores()->find($this->store_id);
+                if (! $store) {
+                    $validator->errors()->add(
+                        'store_id',
+                        'The store does not exist.'
+                    );
+                }
+            },
         ];
     }
 }
