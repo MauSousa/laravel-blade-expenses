505b4ff78d80e558cf88e9c29e982eb03c07210b
Mauricio Sousa <lagrange.tesla@gmail.com>
2025-11-01 18:44:10 -0600

    feat: add expense request validation

--- app/Http/Requests/StoreExpenseRequest.php
+++ app/Http/Requests/StoreExpenseRequest.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace App\Http\Requests;
 
+use Closure;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Validator;
 
 class StoreExpenseRequest extends FormRequest
 {
@@ -13,7 +15,7 @@ class StoreExpenseRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +26,44 @@ class StoreExpenseRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'store_id' => ['required', 'exists:stores,id'],
+            'name' => ['required', 'string', 'max:255'],
+            'price' => ['required', 'numeric', 'min:0'],
+            'payment_method' => ['required', 'string', 'max:255', 'in:cash,check,credit,debit'],
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'store_id.required' => 'The store is required.',
+            'store_id.exists' => 'The store does not exist.',
+            'payment_method.in' => 'The payment method must be one of cash, check, credit, or debit.',
+        ];
+    }
+
+    /**
+     * Checks if the request store belongs to the user.
+     *
+     * @return array<int, Closure>
+     */
+    public function after(): array
+    {
+        return [
+            function (Validator $validator): void {
+                $store = request()->user()->stores()->find($this->store_id);
+                if (! $store) {
+                    $validator->errors()->add(
+                        'store_id',
+                        'The store does not exist.'
+                    );
+                }
+            },
         ];
     }
 }
